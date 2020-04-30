100b15e0cd0fa6661cf1b558b9515c2280312e0c
Claudio Vargas <31100054+SrDarus@users.noreply.github.com>
2020-04-30 15:56:34 -0400

    se agrega el componente navbar, login y nuevo usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// MATERIAL ANGULAR
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { NuevoUsuarioComponent } from './components/login/nuevo-usuario/nuevo-usuario.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    NuevoUsuarioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
